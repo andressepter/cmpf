3200881cbd02e3ace664f4fa15e569a7549bb6fd
Andres Septer <andres.septer@gmail.com>
2025-09-10 12:38:23 +0300

    mall

---
+++ mall.py
@@ -0,0 +1,3 @@
+import cmath      # Kompleksarvude matemaatilised funktsioonid (sqrt, exp, log, trig)
+import math       # Reaalarvude funktsioonid (vajalik nt. abs või nurkade teisendamiseks)
+import numpy as np  # Vektorite ja massiivide kompleksarvuline tugi, lineaaralgebra
